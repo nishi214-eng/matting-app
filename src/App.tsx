414aad56bef6510e34868cff284b6df5a805cfa1
momo_wwwa <109466583+momo-wwwa@users.noreply.github.com>
2024-10-02 02:16:07 +0900

    [add]画像アップロード #3

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChatList from './ChatList';
+import LicenseUpload from './LicenseUpload';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>運転免許証のアップロード</h1>
+      <LicenseUpload />
       </header>
       <ChatList />
     </div>
