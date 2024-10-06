0c22ae328f3f02e66841f9f87e1e4e6c9a260e31
nishi214-eng <89783904+nishi214-eng@users.noreply.github.com>
2024-10-07 05:29:25 +0900

    [chore]react-router-domのインストールとルーティング #9

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <SignIn/>
-        <SignUp/>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="*" element={<h1>Not Found Page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
