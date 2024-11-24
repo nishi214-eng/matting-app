58bbc31c59550bc95e9a3ed5d319510b44fcccbd
momo_wwwa <109466583+momo-wwwa@users.noreply.github.com>
2024-11-25 00:50:28 +0900

    [add] マッチングアプリ機能の追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import ChatList from './pages/ChatList';
 import ProfileForm from './pages/ProfileForm';
 import ProfileDisplay from './pages/ProfileDisplay';
 import Chat from './pages/Chat';
+import Home from './pages/MatchingPage';
 import { AuthProvider } from './store/AuthContext';
 import { AlertProvider } from './store/useSnackber';
 
@@ -23,6 +24,7 @@ function App() {
               <Route path="/" element={<SignIn />} />
               <Route path="/SignUp" element={<SignUp />} />
               <Route path="/ResetPassword" element={<ResetPassword />} />
+              <Route path="/Home" element={<Home/>} />
               <Route path="/ChatList" element={<ChatList />} />
               <Route path="/Chat" element={<Chat/>} />
               <Route path="/ProfileForm" element={<ProfileForm />} />
